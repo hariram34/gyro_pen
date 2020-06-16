05acbe9b03ca44074bcc4f3d757e17899f23224d
hariram34 <hariram.balaji@gmail.com>
2020-06-16 19:22:01 -0400

    Update gyropen.ino

--- gyropen.ino
+++ gyropen.ino
@@ -1,17 +1,21 @@
-
-
-#include <Wire.h>
 #include <I2Cdev.h>
+#include <SoftwareSerial.h>
 #include <MPU6050.h>
+char data [50];
+//int button = 8;
+int click_status;
 
+//const int rxpin = 6, txpin = 7;
+//SoftwareSerial bluetooth(rxpin, txpin);
 
 MPU6050 mpu;
 int16_t ax, ay, az, gx, gy, gz;
 int vx, vy;
 
+
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+ // bluetooth.begin(38400);
+  pinMode(button, INPUT_PULLUP);
   mpu.initialize();
   if (!mpu.testConnection()) {
     while (1);
@@ -21,15 +25,14 @@ void setup() {
 void loop() {
   mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
- Serial.println("a/g:\t");
- Serial.print(ax); Serial.print("\t");
- Serial.print(ay); Serial.print("\t");
- Serial.print(az); Serial.print("\t");
- Serial.print(gx); Serial.print("\t");
- Serial.print(gy); Serial.print("\t");
- Serial.println(gz);
-      
+  //vx = (gy+100)/200;  
+  //vy = -(gz-100)/200; 
 
- 
-  delay(20);
+//click_status = digitalRead(button);
+click_status = 0;
+
+sprintf (data, "%d,%d,%d,%d", gx, gy,gz, click_status);
+//bluetooth.println(data);
+delay(20);
+//bluetooth.flush();
 }
